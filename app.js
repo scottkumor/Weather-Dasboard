076ca0078fce09adb9293dff99facd1b5076b354
unknown <scott.kumor1212@gmail.com>
2020-06-15 18:01:47 -0500

    code clean, no styles

--- app.js
+++ app.js
@@ -17,27 +17,19 @@
 
 $(document).ready(function () {
     let storedHistory = JSON.parse(localStorage.getItem("storage")) || {};
+    
     for (i = 0; i < storedHistory.length; i++) {
         if (Object.keys(storedHistory).length) {
-            $('#history').append(`<button class="bg-g-fbh bd-n c-pri-2 fz-l p-s ta-l ti-s m-s">${storedHistory[i].value}</button>`);
+            $("#history").append(
+                `<button class="historyBtn">${storedHistory[i].value}</button>`
+            );
         }
-    };
-
-    let searches = [];
-
-    function searchStore() {
-        let userInput = $('#cityInput');
-        searches[searches.length] = {
-            value: userInput.val()
-        };
-        localStorage.setItem("storage", JSON.stringify(searches));
-        $('#history').append(`<button class="bg-g-fbh bd-n ol-n c-pri-2 fz-l p-s ta-l ti-s m-s">${userInput.val()}</button>`);
-        $("#cityInput").val('');
-    };
-
-    $('#cityInput').keypress(function (e) {
-        if (e.which == 13) {//Enter key pressed
-            $('#citySubmit').click();//Trigger search button click event
+    }
+    
+    $("#cityInput").keypress(function (e) {
+        if (e.which == 13) {
+            //Enter key pressed
+            $("#citySubmit").click(); //Trigger search button click event
         }
     });
 
@@ -45,135 +37,150 @@ $(document).ready(function () {
     $("#citySubmit").on("click", function () {
         callAPI();
         searchStore();
-        });
-
-    $(document).on("click", ".bg-g-fbh", function () {
-        let pass = $(this).text();
-        $('#cityInput').val(pass);
-        callAPI();
-        $("#cityInput").val('');
     });
 
     $("#clear").on("click", function () {
         localStorage.clear();
-        $('#history').empty();
+        $("#history").empty();
+    });
+    
+    $(document).on("click", ".historyBtn", function () {
+        let pass = $(this).text();
+        $("#cityInput").val(pass);
+        callAPI();
+        $("#cityInput").val("");
+    });
+});
+
+function searchStore() {
+    let searches = [];
+    let userInput = $("#cityInput");
+    searches[searches.length] = {
+        value: userInput.val(),
+    };
+    localStorage.setItem("storage", JSON.stringify(searches));
+    $("#history").append(`<button class="historyBtn">${userInput.val()}</button>`);
+    $("#cityInput").val("");
+}
+
+function changeFavicon(src) {
+    $('link[rel="icon"]').attr("href", src);
+}
+
+function callAPI() {
+    
+    //create space for forecasting
+    let forecastTitle = $(`<div>Your 5-Day Weather Forecast</div>`);
+    let cardsWrapper = $(`<div id="cardsWrapper"></div>`);
+
+    $("#forecastWrapper").append(forecastTitle, cardsWrapper);
+
+    // This is my API key
+    let APIKey = "1bc8de1510a7bc2ef6cbcd528035eef8";
+    let cityInput = $("#cityInput").val().trim();
+
+    // URL needed to query the database
+    let weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=imperial&appid=${APIKey}`;
+
+    // api request for today's weather
+    $.ajax({
+        url: weatherURL,
+        method: "GET",
+    }).then(function (weatherRes) {
+        // Log the resulting object
+        //console.log(weatherRes);
+
+        let timeStamp = weatherRes.dt;
+        let newDate = moment.unix(timeStamp).format("L");
+        let iCode = weatherRes.weather[0].icon;
+        let iURL = "http://openweathermap.org/img/wn/" + iCode + "@2x.png";
+        changeFavicon(`${iURL}`);
+
+        //console.log(newDate);
+
+        $("#weatherName").html(`<div>${weatherRes.name}</div>`);
+        $("#weatherDate").html(`<div>${newDate}</div>`);
+        $("#weatherTemp").html(
+            `<div class="">The current temperature is</div><div class="">${weatherRes.main.temp.toFixed() + "°"}</div>`
+        );
+        $("#weatherHum").html(
+            `<div class="">The current humidity is</div><div class="">${weatherRes.main.humidity.toFixed() + "%"}</div>`
+        );
+        $("#weatherWind").html(
+            `<div class="">Wind gusts could reach</div><div class="">${weatherRes.wind.speed.toFixed() + " mph"} </div>`
+        );
+        $("#weatherIcon").html(`<img class="" src="${iURL}"/>`);
+
+        let uvLat = weatherRes.coord.lat;
+        let uvLon = weatherRes.coord.lon;
+        let uvURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${APIKey}&lat=${uvLat}&lon=${uvLon}`;
+
+
+        // seprate call for UV
+        $.ajax({
+            url: uvURL,
+            method: "GET",
+        }).then(function (uvRes) {
+            // Log the resulting object
+            //console.log(uvRes);
+            $("#weatherUV").html(
+                `<div class="">The current<a class="" href"https://www.aimatmelanoma.org/prevention/uv-index/"> UV Index</a> is</div><div class="c-pri-2 fz-jj p-s ts-n">${uvRes.value}</div>`
+            );
+        });
     });
 
-    function changeFavicon(src) {
-        $('link[rel="icon"]').attr('href', src);
-    }
+// synchronous request for forecast data
 
-    function callAPI() {
+    let forecastURL =
+        "http://api.openweathermap.org/data/2.5/forecast?q=" +
+        cityInput +
+        "&appid=" +
+        APIKey;
 
-        let forecastTitle = $(`<div class="fz-jj c-pri-3 fs-i">Your 5-Day Weather Forecast</div>`)
-        $('#forecastTitle').html(forecastTitle);
-        $('#forecast').html('');
-        
+    $.ajax({
+        url: forecastURL,
+        method: "GET",
+    }).then(function (forecastRes) {
+        $("#cardsWrapper").empty();
 
-        // This is my API key
-        let APIKey = "1bc8de1510a7bc2ef6cbcd528035eef8";
-        let cityInput = $('#cityInput').val().trim();
+        newDiv = "";
 
-        // Here we are building the URLs we need to query the database
-        let queryURLw = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=imperial&appid=${APIKey}`;
+        let list = forecastRes.list;
 
-        //performing an ajax request
-        $.ajax({
-            url: queryURLw,
-            method: "GET"
-        }).then(function (responseW) {
-            // Log the resulting object
-            //console.log(responseW);
+        list.forEach((num, index) => {
+            //console.log(index);
+            if (index % 8 === 0) {
+            /* create card  and append to html */
+                let K = list[index].main.temp;
+                let lK = list[index].main.temp_min;
+                let hK = list[index].main.temp_max;
+                let dt = list[index].dt;
+                let date = moment.unix(dt).format("L");
+            //   console.log(date);
 
-            let timeStamp = responseW.dt;
-            let newDate = moment.unix(timeStamp).format('L');
-            let iCode = responseW.weather[0].icon;
-            let iURL = "http://openweathermap.org/img/wn/" + iCode + "@2x.png";
-            changeFavicon(`${iURL}`);
+                let iconGet = list[index].weather[0].icon;
+                //let subIconGet = iconGet[index].icon;
+                //console.log(iconGet);
 
-            //console.log(newDate);
+                let icon = "http://openweathermap.org/img/wn/" + iconGet + "@2x.png";
+                let temp = ((K - 273.15) * 1.8 + 32).toFixed();
+                let hum = list[index].main.humidity.toFixed();
+                let low = ((lK - 273.15) * 1.8 + 32).toFixed();
+                let high = ((hK - 273.15) * 1.8 + 32).toFixed();
 
-            $('#weatherName').html(`<div>${responseW.name}</div>`);
-            $('#weatherDate').html(`<div>${newDate}</div>`)
-            $('#weatherTemp').html(`<div class="ff-3 fs-i">The current temperature is</div><div class="c-pri-2 fz-jjj p-s ts-n">${responseW.main.temp.toFixed() + '°'}</div>`);
-            $('#weatherHum').html(`<div class="ff-3 fs-i">The current humidity is</div><div class="c-pri-2 fz-jjj p-s ts-n">${responseW.main.humidity.toFixed() + '%'}</div>`);
-            $('#weatherWind').html(`<div class="ff-3 fs-i">Wind gusts could reach</div><div class="c-pri-2 fz-jj p-s ts-n">${responseW.wind.speed.toFixed() + ' mph'} </div>`);
-            $('#weatherIcon').html(`<img class="iz-i bgc-pri-5 s" src="${iURL}"/>`)
+                newDiv = $("<div>");
 
-            let uvLat = responseW.coord.lat;
-            let uvLon = responseW.coord.lon;
-            let queryURLu = `http://api.openweathermap.org/data/2.5/uvi?appid=${APIKey}&lat=${uvLat}&lon=${uvLon}`;
+                newDiv.html(
+                    `<div class="">${date}</div><div class="">${temp}°</div><img src="${icon}"><br />Humidity: ${hum}%<br />Low: ${low}°<br />High: ${high}°`
+                );
+                newDiv.attr("class", "");
 
-            $.ajax({
-                url: queryURLu,
-                method: "GET"
-            }).then(function (responseU) {
-                // Log the resulting object
-                //console.log(responseU);
-                $('#weatherUV').html(`<div class="ff-3 fs-i">The current<a class="td-n c-pri-3" href"https://www.aimatmelanoma.org/prevention/uv-index/"> UV Index</a> is</div><div class="c-pri-2 fz-jj p-s ts-n">${responseU.value}</div>`);
-
-            });
+                $("#cardsWrapper").append(newDiv);
+            } else {
+            // go through heach item
+            //grab temp, humidity, low, high, and icon
+            //average the temps, lowest low, highest high
+            }
         });
-
-
-        let queryURLf = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=" + APIKey;
-
-        $.ajax({
-            url: queryURLf,
-            method: "GET"
-        }).then(function (response) {
-
-            $('#cardsWrapper').empty();
-
-            newDiv = '';
-
-            let list = response.list;
-
-            list.forEach((num, index) => {
-                //console.log(index);
-                if (index%8 === 0) {
-                  /* create card  and append to html */
-                  let K = list[index].main.temp;
-                  let lK = list[index].main.temp_min;
-                  let hK = list[index].main.temp_max;
-                  let dt = list[index].dt
-                  let date = moment.unix(dt).format('L');
-                                  console.log(date);
-
-                 
-                  let iconGet = list[index].weather[0].icon;
-                  //let subIconGet = iconGet[index].icon;
-                  //console.log(iconGet);
-  
-                  let icon = "http://openweathermap.org/img/wn/" + iconGet + "@2x.png";
-                  // console.log(icon);
-                  //<img src="${icon}"> <-- append inside newdiv.html
-  
-                  let temp = (((K - 273.15) * 1.8) + 32).toFixed();
-                  let hum = list[index].main.humidity.toFixed();
-                  let low = (((lK - 273.15) * 1.8) + 32).toFixed();
-                  let high = (((hK - 273.15) * 1.8) + 32).toFixed();
-  
-                  
-  
-                  newDiv = $('<div>');
-  
-                  newDiv.html(`<div class="c-g-wdb fz-j">${date}</div><div class="fz-jj">${temp}°</div><img src="${icon}"><br />Humidity: ${hum}%<br />Low: ${low}°<br />High: ${high}°`);
-                  newDiv.attr("class", "bgc-pri-5 d-f df-fdc jc-c ai-c m-s p-m s");
-  
-                  $('#cardsWrapper').append(newDiv);
-                }
-
-                else {
-                    // go through heach item
-                    //grab temp, humidity, low, high, and icon
-                    //average the temps, lowest low, highest high
-                }
-              });
-                
-               
-
-            
-        });
-    }
-});
+    });
+}
